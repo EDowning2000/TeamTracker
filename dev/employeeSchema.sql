27d74b29d5b54bb5ea7cfadd7fa2a8d1d085f729
EDowning2000 <emersondowning@gmail.com>
2020-03-27 16:21:05 -0400

    typo's fixed in schema and seed files.

--- dev/employeeSchema.sql
+++ dev/employeeSchema.sql
@@ -29,8 +29,8 @@ CREATE TABLE employee (
 INSERT INTO department (name)
 VALUES ('Management'),('Sales'),('Accounting');
 
-INSERT INTO role (title, salary, title_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Regional Manager', 80000, 1),('Assistant to Regional Manager', 60000, 2),('Salesman', 60000, 3),('Accountant',40000,4);
 
-INSERT INTO employee (firstName, lastName, roleID, managerID)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Michael', 'Scott', 1, null),('Dwight','Schrute',2,null),('Jim','Halpert', 3, null),('Stanley', 'Hudson', 3,null),('Angela','Schrute',4,null),('Kevin', 'Malone',4,null);
